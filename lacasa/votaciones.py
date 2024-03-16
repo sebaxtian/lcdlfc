adb01b6208889f856cb75c4376429fbf88f27783
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2024-03-15 20:50:56 -0700

    Proceso de ejecutar votaciones sobre los targets con prioridades

--- lacasa/votaciones.py
+++ lacasa/votaciones.py
@@ -1,6 +1,8 @@
 import csv
 import random
 import time
+from enum import Enum
+from typing import Dict
 
 from icecream import ic
 from loguru import logger
@@ -13,12 +15,20 @@ from cfg.webbrowser import WebBrowser, WebBrowserType
 ic.configureOutput(includeContext=True)
 
 
+class Prioridad(Enum):
+    BAJA = 1
+    MEDIA = 2
+    ALTA = 3
+
+
 class Target:
 
-    def __init__(self, id_target: str, css_selector: str, tipo_votacion: str, votos: int = 0):
+    def __init__(self, id_target: str, css_selector: str, tipo_votacion: str, prioridad: Prioridad = Prioridad.ALTA,
+                 votos: int = 0):
         self.id_target = id_target
         self.css_selector = css_selector
         self.tipo_votacion = tipo_votacion
+        self.prioridad = prioridad
         self.votos = votos
 
     def get_votos(self):
@@ -28,10 +38,10 @@ class Target:
         self.votos += 1
 
     def __str__(self):
-        return f"{{ID_TARGET: {self.id_target}, CSS_SELECTOR: {self.css_selector}, TIPO_VOTACION: {self.tipo_votacion}, VOTOS: {self.votos}}}"
+        return f"{{ID_TARGET: {self.id_target}, CSS_SELECTOR: {self.css_selector}, TIPO_VOTACION: {self.tipo_votacion}, PRIORIDAD: {self.prioridad}, VOTOS: {self.votos}}}"
 
     def __repr__(self):
-        return f"{{ID_TARGET: {self.id_target}, CSS_SELECTOR: {self.css_selector}, TIPO_VOTACION: {self.tipo_votacion}, VOTOS: {self.votos}}}"
+        return f"{{ID_TARGET: {self.id_target}, CSS_SELECTOR: {self.css_selector}, TIPO_VOTACION: {self.tipo_votacion}, PRIORIDAD: {self.prioridad}, VOTOS: {self.votos}}}"
 
 
 class Votaciones:
@@ -39,31 +49,56 @@ class Votaciones:
 
     def __init__(self, multiple: bool = False):
         self.multiple = multiple
-        self.targets = self._load_targets()
         if osenv.get("WEBBROWSER_TYPE") == WebBrowserType.FIREFOX.value:
             self.wb = WebBrowser(type_of_browser=WebBrowserType.FIREFOX)
         else:
+            # Chrome
             self.wb = WebBrowser()
 
-    def _load_targets(self) -> [Target]:
-        targets = []
+    @staticmethod
+    def load_targets() -> Dict[str, Target]:
+        targets: Dict[str, Target] = {}
+
         with open(osenv.get("CSV_TARGETS"), "r") as csv_file:
             csv_reader = csv.DictReader(csv_file)
-            [targets.append(Target(id_target=fila['ID_TARGET'], css_selector=fila['CSS_SELECTOR'],
-                                   tipo_votacion=fila['TIPO_VOTACION'])) for fila in
-             csv_reader]
+            for fila in csv_reader:
+                match fila['PRIORIDAD']:
+                    case "BAJA":
+                        p = Prioridad.BAJA
+                    case "MEDIA":
+                        p = Prioridad.MEDIA
+                    case "ALTA":
+                        p = Prioridad.ALTA
+                    case _:
+                        p = Prioridad.BAJA
+
+                t = Target(id_target=fila['ID_TARGET'], css_selector=fila['CSS_SELECTOR'],
+                           tipo_votacion=fila['TIPO_VOTACION'], prioridad=p)
+
+                targets[fila['ID_TARGET']] = t
 
         return targets
 
-    def votar(self, target: Target = None) -> Target:
+    @staticmethod
+    def choice_target(targets: Dict[str, Target]):
+        # Selecciona aleatoriamente un target usando los pesos de las prioridades
+        prioridades = [t[1].prioridad.value for t in list(targets.items())]
+        return random.choices(list(targets.items()), weights=prioridades)[0][1]
+
+    def votar(self) -> Dict[str, Target]:
         self.wb.open()
         self.wb.go_to(self.URL_VOTACIONES)
         self.wb.driver.implicitly_wait(2)
-        target = random.choice(self.targets) if not target else target
+
+        # Los targets son cargados una sola vez al llamar el metodo
+        targets = Votaciones.load_targets()
+
+        # Selecciona aleatoriamente un target usando los pesos de las prioridades
+        target = Votaciones.choice_target(targets)
 
         try:
-
             btn_target = self.wb.driver.find_element(By.CSS_SELECTOR, target.css_selector)
+            # time.sleep(10)
             btn_target.click()
 
             time.sleep(1)
@@ -72,12 +107,19 @@ class Votaciones:
             # TODO: Verificar que el voto ha sido exitoso antes de sumar voto
             target.sumar_voto()
 
+            # Actualiza el target en los targets
+            targets[target.id_target] = target
+
             while self.multiple:
                 self.wb.open()
                 self.wb.go_to(self.URL_VOTACIONES)
+                self.wb.driver.implicitly_wait(2)
 
-                target = random.choice(self.targets)
+                # Selecciona aleatoriamente un target usando los pesos de las prioridades
+                target = Votaciones.choice_target(targets)
+
                 btn_target = self.wb.driver.find_element(By.CSS_SELECTOR, target.css_selector)
+                # time.sleep(10)
                 btn_target.click()
 
                 time.sleep(1)
@@ -86,8 +128,11 @@ class Votaciones:
                 # TODO: Verificar que el voto ha sido exitoso antes de sumar voto
                 target.sumar_voto()
 
+                # Actualiza el target en los targets
+                targets[target.id_target] = target
+
         except NoSuchElementException:
             logger.warning(f"No se encuentra target: {target}")
             self.wb.quit()
         finally:
-            return target
+            return targets
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import csv
 from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import wait
+from typing import Dict
 
 import pendulum
 from icecream import ic
@@ -10,11 +11,17 @@ from loguru import logger
 from cfg import osenv
 from lacasa.votaciones import Votaciones, Target
 
+
+def set_logger_datetime(record):
+    record['time'] = pendulum.now("America/Bogota")
+
+
+logger.configure(patcher=set_logger_datetime)
 logger.add(
-    "logs/lcdlfc_{time:!UTC}.log",
+    f"logs/lcdlfc_{pendulum.now("America/Bogota").to_iso8601_string()}.log",
     rotation="20 MB",
     level="DEBUG",
-    format="{time:!UTC} - {level} - {name} - {message}",
+    format="{time} - {level} - {name} - {message}",
 )
 
 ic.configureOutput(includeContext=True)
@@ -24,17 +31,18 @@ ic.configureOutput(includeContext=True)
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def export_data(targets: [Target]) -> None:
+def export_data(targets: [Dict[str, Target]]) -> None:
     now_iso8601 = pendulum.now("America/Bogota").to_iso8601_string()
     filename = f"data/targets_{now_iso8601}.csv"
 
     with open(filename, "w") as csv_file:
-        header = ["DATETIME", "ID_TARGET", "CSS_SELECTOR", "TIPO_VOTACION", "VOTOS"]
+        header = ["DATETIME", "ID_TARGET", "CSS_SELECTOR", "TIPO_VOTACION", "PRIORIDAD", "VOTOS"]
         writer = csv.DictWriter(csv_file, fieldnames=header)
         writer.writeheader()
         for target in targets:
             target = target.__dict__
             target = {key.upper(): value for key, value in target.items()}
+            target['PRIORIDAD'] = str(target['PRIORIDAD']).split('.')[1]
             target['DATETIME'] = now_iso8601
             writer.writerow(target)
 
@@ -43,11 +51,13 @@ def export_data(targets: [Target]) -> None:
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    num_threads = int(osenv.get("NUM_THREADS"))
+    num_threads = int(osenv.get("NUM_THREADS")) if int(osenv.get("NUM_THREADS")) < 9 else 8
     multiples_votos = True if osenv.get("MULTIPLES_VOTOS") == "True" else False
+    targets: [Target] = Votaciones.load_targets()
 
     logger.info(f"Numero de threads: {num_threads}")
     logger.info(f"Multiples votos: {multiples_votos}")
+    ic(targets)
 
     # Create a ThreadPoolExecutor with maximum 8 threads
     with ThreadPoolExecutor(max_workers=num_threads) as executor:
@@ -67,11 +77,11 @@ if __name__ == '__main__':
         [logger.info(f"{key} : {thread.result()}") for key, thread in threads.items()]
 
         # Obtener el total de votos
-        total_votos = sum(thread.result().votos for thread in threads.values())
+        total_votos = sum(target.votos for thread in threads.values() for target in thread.result().values())
         ic(total_votos)
 
         # Logger total votos
         logger.info(f"Total votos: {total_votos}")
 
         # Export data
-        export_data([thread.result() for thread in threads.values()])
+        export_data([target for thread in threads.values() for target in thread.result().values()])
